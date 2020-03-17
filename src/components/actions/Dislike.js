0219ce687d81f25329fbbc7b5c629742cd8eab95
dongsubak <likeap@catholic.ac.kr>
2020-03-17 18:45:07 +0900

    Dislike

--- src/components/actions/Dislike.js
+++ src/components/actions/Dislike.js
@@ -1,3 +1,12 @@
-const Dislike = () => {return null};
+import React from 'react';
+
+const Dislike = ({person, modifySuperficialChoices}) => (
+    <button 
+        type="button"
+        onClick={() => modifySuperficialChoices(userId, 'ADD_TO_DISLIKED_USER')}
+    >
+        <img src="/images/misc/dislike.png" alt="Dislike User" />
+    </button>
+    );
 
 export default Dislike;
